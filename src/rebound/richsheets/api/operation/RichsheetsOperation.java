c20de9b120fcfe8146caae24fba55d480dc6ebe7
PuppyPi <puppyofcodez@gmail.com>
2022-05-28 16:00:57 +0000

    Error in Javadoc

--- src/rebound/richsheets/api/operation/RichsheetsOperation.java
+++ src/rebound/richsheets/api/operation/RichsheetsOperation.java
@@ -9,8 +9,8 @@ import rebound.richsheets.api.model.RichsheetsTable;
 public interface RichsheetsOperation
 {
 	/**
-	 * @param data  this is created especially for this invocation and never used by Datashets again (so feel free to keep hold of it after {@link RichsheetsConnection#perform(RichsheetsOperation)} or similar returns!
-	 * @return null meaning don't write anything, otherwise it may be the same Java Object (referencewise identical) as was given to it or not!  (whatever is return is only used briefly, then forgotten once {@link RichsheetsConnection#perform(RichsheetsOperation)} or similar returns, so feel free to keep hold of it afterward or have brought it in from beforehand!)
+	 * @param data  this is created especially for this invocation and never used by Datashets again (so feel free to keep hold of it after {@link RichsheetsConnection#perform(Integer, RichsheetsOperation)} or similar returns!
+	 * @return null meaning don't write anything, otherwise it may be the same Java Object (referencewise identical) as was given to it or not!  (whatever is return is only used briefly, then forgotten once {@link RichsheetsConnection#perform(Integer, RichsheetsOperation)} or similar returns, so feel free to keep hold of it afterward or have brought it in from beforehand!)
 	 * @throws RuntimeException  anything thrown by this will be caught and handled properly (maintenance will write its changes, but no client code changes will be written)
 	 */
 	public @Nullable RichsheetsWriteData performInMemory(@Nonnull RichsheetsTable data) throws RuntimeException;
