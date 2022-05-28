91d9d1586804060b3bb5a320beee2670b6c2662d
PuppyPi <puppyofcodez@gmail.com>
2022-05-28 17:14:55 +0000

    Can output null table data to ask to *only* resize columns!  :3 (Or to do Other Things if resize columns is empty!)

--- src/rebound/richsheets/api/operation/RichsheetsWriteData.java
+++ src/rebound/richsheets/api/operation/RichsheetsWriteData.java
@@ -1,18 +1,20 @@
 package rebound.richsheets.api.operation;
 
+import static java.util.Objects.*;
 import java.util.Set;
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import rebound.richsheets.api.model.RichsheetsTable;
 
 public class RichsheetsWriteData
 {
-	protected final @Nonnull RichsheetsTable table;
+	protected final @Nullable RichsheetsTable table;
 	protected final @Nonnull Set<Integer> columnsToAutoresize;  //zero-based indexes; can be empty but not null
 	
 	public RichsheetsWriteData(RichsheetsTable table, Set<Integer> columnsToAutoresize)
 	{
 		this.table = table;
-		this.columnsToAutoresize = columnsToAutoresize;
+		this.columnsToAutoresize = requireNonNull(columnsToAutoresize);
 	}
 	
 	public RichsheetsTable getTable()
