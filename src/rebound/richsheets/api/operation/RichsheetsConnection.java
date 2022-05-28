f4c8dbee7b58c96b4122733edd44b553a34ed8b8
PuppyPi <puppyofcodez@gmail.com>
2022-05-28 16:44:35 +0000

    Special method that can be overridden to quickly just get the timestamp of the data!

--- src/rebound/richsheets/api/operation/RichsheetsConnection.java
+++ src/rebound/richsheets/api/operation/RichsheetsConnection.java
@@ -2,8 +2,10 @@ package rebound.richsheets.api.operation;
 
 import static java.util.Collections.*;
 import java.io.IOException;
+import java.util.Date;
 import javax.annotation.Nullable;
 import rebound.richsheets.api.model.RichsheetsTable;
+import rebound.richsheets.api.operation.RichsheetsOperation.RichsheetsOperationWithDataTimestamp;
 
 public interface RichsheetsConnection
 {
@@ -22,6 +24,19 @@ public interface RichsheetsConnection
 	{
 		perform(null, d -> new RichsheetsWriteData(contents, emptySet()));  //if reading the sheet is actually unnecessary if not used by the client code (and thus inefficient)..then override write()!  XD  :3
 	}
+	
+	
+	/**
+	 * @return null if unknown (an exception is thrown like it would be in {@link #read(Integer)} if the data isn't there!)
+	 */
+	public default @Nullable Date getCurrentLastModifiedTimestamp() throws IOException
+	{
+		//if reading the sheet is unnecessary if not used by the client code (and thus inefficient)..then override write()!  XD  :3
+		Date[] c = new Date[1];
+		perform(0, (RichsheetsOperationWithDataTimestamp)(d, t) -> {c[0] = t; return null;});
+		return c[0];
+	}
+	
 	
 	
 	public void perform(@Nullable Integer maxRowsToRead, @Nullable RichsheetsOperation operation) throws IOException;
