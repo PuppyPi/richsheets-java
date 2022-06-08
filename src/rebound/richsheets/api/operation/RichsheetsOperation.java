60af14640609a1738803b762a52c4b81d8e3ee76
PuppyPi <puppyofcodez@gmail.com>
2022-06-08 23:40:52 +0000

    Oops, forgot to commit this; apparently I swapped Nullable with Nonnull! ^^'

--- src/rebound/richsheets/api/operation/RichsheetsOperation.java
+++ src/rebound/richsheets/api/operation/RichsheetsOperation.java
@@ -1,7 +1,6 @@
 package rebound.richsheets.api.operation;
 
 import java.util.Date;
-import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import rebound.richsheets.api.model.RichsheetsTable;
 
@@ -9,18 +8,18 @@ import rebound.richsheets.api.model.RichsheetsTable;
 public interface RichsheetsOperation
 {
 	/**
-	 * @param data  this is created especially for this invocation and never used by Datashets again (so feel free to keep hold of it after {@link RichsheetsConnection#perform(Integer, RichsheetsOperation)} or similar returns!
+	 * @param data  this is created especially for this invocation and never used by Datashets again (so feel free to keep hold of it after {@link RichsheetsConnection#perform(Integer, RichsheetsOperation)} or similar returns!   (null means it's absent and if you return a value, a backing store will be created!)
 	 * @return null meaning don't write anything, otherwise it may be the same Java Object (referencewise identical) as was given to it or not!  (whatever is return is only used briefly, then forgotten once {@link RichsheetsConnection#perform(Integer, RichsheetsOperation)} or similar returns, so feel free to keep hold of it afterward or have brought it in from beforehand!)
 	 * @throws RuntimeException  anything thrown by this will be caught and handled properly (maintenance will write its changes, but no client code changes will be written)
 	 */
-	public @Nullable RichsheetsWriteData performInMemory(@Nonnull RichsheetsTable data) throws RuntimeException;
+	public @Nullable RichsheetsWriteData performInMemory(@Nullable RichsheetsTable data) throws RuntimeException;
 	
 	
 	
 	public static interface RichsheetsOperationWithDataTimestamp
 	extends RichsheetsOperation
 	{
-		public @Nullable RichsheetsWriteData performInMemory(@Nonnull RichsheetsTable data, @Nullable Date lastModifiedTimeOfOriginalData) throws RuntimeException;
+		public @Nullable RichsheetsWriteData performInMemory(@Nullable RichsheetsTable data, @Nullable Date lastModifiedTimeOfOriginalData) throws RuntimeException;
 		
 		@Override
 		public default RichsheetsWriteData performInMemory(RichsheetsTable data) throws RuntimeException
